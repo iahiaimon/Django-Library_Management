4b19977687adb0dac8c3264b2be1cb90df2baf59
Iahia Imon <iahiaimon555@gmail.com>
2025-12-05 00:45:26 +0600

    Customuser View

--- Root_App/urls.py
+++ Root_App/urls.py
@@ -20,5 +20,6 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("reload/", include("django_browser_reload.urls")),
-    path("" , include("books.urls"))
+    path("" , include("books.urls")),
+    path("user/" , include("accounts.urls")),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+from .models import CustomUser
+from .forms import CustomUserForm
+
+def UserSignupView(request):
+    # if GET -> show empty form
+    if request.method == "GET":
+        form = CustomUserForm()
+        return render(request, "accounts/signup.html", {"form": form})
+
+    # if POST -> validate and save
+    form = CustomUserForm(request.POST)
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data["password"])  # hash password
+        user.save()  # save the instance we modified
+
+        messages.success(request, f"User {user.username} created successfully.")
+        return redirect("home")  # change "home" to your URL name
+    else:
+        # form invalid -> re-render with errors
+        return render(request, "accounts/signup.html", {"form": form})
