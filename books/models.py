44831fb14c08eeb811a02057b09beccffcf30471
Iahia Imon <iahiaimon555@gmail.com>
2025-12-07 01:51:33 +0600

    category - books model modify

--- books/models.py
+++ books/models.py
@@ -4,18 +4,28 @@ from core.models.time_stamp import TimeStampModel
 # Create your models here.
 
 
+from django.db import models
+from django.utils.text import slugify
+
 class Category(TimeStampModel):
-    title = models.CharField(blank=False, null=False)
-    slug = models.SlugField(max_length=255)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+    cat_image = models.ImageField(upload_to="Category_Image" , default = "Category_Image/default_category.jpg")
     description = models.TextField()
 
+    def save(self, *args, **kwargs):
+        if not self.slug:  # only set slug if empty
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
     def __str__(self):
-        return f"{self.title}"
+        return self.title
+
 
 
 class AddBook(TimeStampModel):
     title = models.CharField(blank=False, null=False)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255 , unique=True , blank= True)
     author = models.CharField(blank=False, null=False)
     description = models.TextField()
     isbn = models.CharField(blank=False, null=False, max_length=13)
@@ -24,6 +34,11 @@ class AddBook(TimeStampModel):
     language = models.CharField(blank=False, null=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     cover_image = models.ImageField(upload_to="Book Cover" , default = "Book Cover/default_cover.jpg")
+
+    def save(self, *args, **kwargs):
+        if not self.slug:  # only set slug if empty
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} -- {self.isbn} by {self.author}"
